be068ebe0074a814ec5b4330502132b41d9f75b2
Phcavalcante46 <amaral.peedroh@gmail.com>
2026-08-10 10:57:25 -0300

    Adiciona varios exemplos de arrays no php

--- 10-loop-para-estrutura-de-dados.php
+++ 10-loop-para-estrutura-de-dados.php
@@ -1,30 +1,123 @@
 <!DOCTYPE html>
 <html lang="pt-bre">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
- </head>
+</head>
+
 <body>
     <div class="container">
         <h1>Loops para estruturas de dados</h1>
         <hr>
 
-        <?php 
+        <?php
         $meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"];
         ?>
 
         <h2>Usando o loop for para acessar o array</h2>
 
-     <ol>
-      <?php for($i = 0; $i < count($meses); $i++){ ?>   
-            <li> <?= $meses[$i] ?> </li>
-    <?php } ?>   
-    </ol>
+        <ol>
+            <?php for ($i = 0; $i < count($meses); $i++) { ?>
+                <li> <?= $meses[$i] ?> </li>
+            <?php } ?>
+        </ol>
+
+        <hr>
+
+        <h2>
+            Usando o loop for para acessar uma matriz (array de arrays)
+        </h2>
+        <?php
+
+        $planoDeEstudos = [
+            ["JS Avançado", "Node.js", "Next.js"],
+            ["PHP", "Orientação a Objetos"],
+            ["Teoria das Cores", "Photoshop com IA", "UX/UI"]
+        ];
+        $linhas = count($planoDeEstudos);
+        for ($i = 0; $i < $linhas; $i++): // acessa cada linha
+            $colunas = count($planoDeEstudos[$i]);
+            for ($j = 0; $j < $colunas; $j++): //acessa cada coluna
+        ?>
+                <p> <?= $planoDeEstudos[$i][$j] ?> </p>
+        <?php
+
+            endfor;
+        endfor;
+        ?>
+
+        <hr>
+
+        <h2>Usando o loop foreach para arrays</h2>
+
+        <?php
+        $alunos = ["Thiago", "Adela", "Renan", "Pérola"];
+        foreach ($alunos as $aluno):
+        ?>
+
+            <p><?= $aluno ?></p>
+        <?php
+        endforeach;
+        ?>
+
+        <hr>
+    
+        <h2> Usando foreach para array associativo </h2>
+
+        <?php
+        $curso = [
+            "titulo" => "Gastronomia",
+            "carga_horaria" => 200,
+            "descricao" => "Aprender o básico sobre a área"
+        ];
+
+        foreach ($curso as $chave => $valor):
+        ?>
+
+            <p><b> <?= $chave ?> </b>: <?= $valor ?> </p>
+        <?php
+
+        endforeach;
+        foreach ($curso as $valor):
+
+        ?>
+
+            <p><i><?= $valor ?></i></p>
+
+        <?php
+        endforeach;
+        ?>
+
+        <hr>
+
+        <h2>Usando o foreach para uma matriz</h2>
+
+        <?php 
+        
+        foreach($planoDeEstudos as $categoria): // cada linha (categoria)
+
+            foreach($categoria as $assunto): //cada coluna (assunto/curso)
+        ?>
+
+        <p><?= $assunto ?></p>
+
+        <?php 
+        
+            endforeach;
+
+        endforeach;
+        
+        ?>
+
+
+
+
 
     </div>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </body>
+
 </html>
- 
