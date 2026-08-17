902dab054952c7ff1547e684a770e7775154d8b0
Phcavalcante46 <amaral.peedroh@gmail.com>
2026-08-17 09:10:09 -0300

    adicionamos algumas formatações de data hora e numeros

--- 13-funcoes-nativas-para-numeros-data-hora.php
+++ 13-funcoes-nativas-para-numeros-data-hora.php
@@ -49,11 +49,11 @@
 
         <h3>number_format()</h3>
 
-        <?php 
-        
+        <?php
+
         $preco = 10567.86;
         $numeroComMuitasCasasDecimais = 1458.4567123;
-        
+
         ?>
 
         <p>Preço formatado:
@@ -64,7 +64,34 @@
             <?= number_format($numeroComMuitasCasasDecimais, 3) ?>
         </p>
 
+        <hr>
 
+        <h2>Data e Hora</h2>
+        <h3>date(), date_default_timezone_set(), strtotime()</h3>
+
+        <?php
+
+        date_default_timezone_set("America/Sao_Paulo");
+        $data = date("d/m/Y");
+        $hora = date("H:i:s");
+        $dataHora = date("d/m/Y H:i")
+
+        ?>
+
+
+        <p>Data: <?= $data ?></p>
+        <p>hora: <?= $hora ?> </p>
+        <p>Data e hora: <?= $dataHora ?></p>
+
+        <?php
+
+        $dataFormatoBD = "2020-09-11";
+
+        /* usamos o strtotime para converter primeiro, a data em um timestamp (data em formato numerico). Em segyuda, o date aplica o formato desejado*/
+        $datFormatada = date("d/m/Y", strtotime($dataFormatoBD));
+        ?>
+        <p>Data (em formato de sistema/banco): <?= $dataFormatoBD ?></p>
+        <p>Data: (convertida e formatada): <?= $datFormatada ?></p>
     </div>
 
 
